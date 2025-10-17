8dc2ac243c10446e7d39cd91f7bae6b9d2732715
Kxria <hugo.chaparro@uabc.edu.mx>
2025-10-17 15:17:41 -0700

    Update pilas

--- Pilas y colas/main.c
+++ Pilas y colas/main.c
@@ -1,58 +1,52 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct Nodo {
+typedef struct _nodo {
     int dato;
-    struct Nodo *ant;   
-};
+    struct _nodo *ant;
+} Nodo;
 
-int vacia(struct Nodo *tope) {
-    return (!tope)? 1:0;
+int vacia(Nodo *tope) {
+    return (tope == NULL);
 }
 
-struct Nodo* crearNodo(int n) {
-    //PASO 1: Creamos el nuevo nodo
-    struct Nodo *nuevo_nodo;
-    nuevo_nodo = (struct Nodo*)malloc(sizeof(struct Nodo));
-    
-    //PASO 2: Asignar el dato al nuevo nodo
-    nuevo_nodo ->dato = n;
-    nuevo_nodo ->ant = NULL;
-    return(nuevo_nodo);
+Nodo* crearNodo(int n) {
+    Nodo *nuevo_nodo = (Nodo*)malloc(sizeof(Nodo));
+    nuevo_nodo->dato = n;
+    nuevo_nodo->ant = NULL;
+    return nuevo_nodo;
 }
 
-struct Nodo* push(struct Nodo *tope, int n) {
-    struct Nodo *aux;
-    aux = crearNodo(n);
-    aux -> ant = tope;
-    tope = aux;
-    return(tope);
+Nodo* push(Nodo *tope, int n) {
+    Nodo *aux = crearNodo(n);
+    aux->ant = tope;
+    return aux;
 }
 
-void mostrar(struct Nodo *tope) {
-    struct Nodo *aux;
-    if(!tope)
-        printf("La pila esta vacia");
-    else {
-        aux = tope;
-        do{
-            printf("%d ", aux->dato);
-            aux = aux->ant;
-        }while(aux != NULL);
+void mostrar(Nodo *tope) {
+    if (vacia(tope)) {
+        printf("La pila esta vacia\n");
+        return;
     }
+    Nodo *aux = tope;
+    while (aux != NULL) {
+        printf("%d ", aux->dato);
+        aux = aux->ant;
+    }
+    printf("\n");
 }
 
-struct Nodo* pop(struct Nodo *tope) {
-    struct Nodo *aux;
-    aux = tope;
+Nodo* pop(Nodo *tope) {
+    Nodo *aux = tope;
     tope = tope->ant;
     free(aux);
-    return(tope);
+    return tope;
 }
 
-void main(void) {
-    struct Nodo *tope = NULL;
+int main(void) {
+    Nodo *tope = NULL;
     int dato, op;
+
     do {
         printf("\nPROGRAMA QUE IMPLEMENTA PILAS CON LISTAS ENLAZADAS\n\n");
         printf("1. Push\n");
@@ -61,33 +55,39 @@ void main(void) {
         printf("4. Salir\n");
         printf("Opcion: ");
         scanf("%d", &op);
+
         switch(op) {
-            case 1: 
+            case 1:
                 printf("Introduce un numero: ");
                 scanf("%d", &dato);
-                if(tope == NULL)
-                    tope = crearNodo(dato);
-                else
-                    tope = push(tope,dato);
+                tope = push(tope, dato);
                 break;
-                
-            case 2: 
-                if(!vacia(tope)) {
+
+            case 2:
+                if (!vacia(tope)) {
                     printf("El dato eliminado es: %d\n", tope->dato);
                     tope = pop(tope);
-                    printf("El nuevo dato en la cima es: %d\n", tope->dato);
-                }
-                else
+                    if (tope != NULL)
+                        printf("El nuevo dato en la cima es: %d\n", tope->dato);
+                    else
+                        printf("La pila ahora esta vacia\n");
+                } else {
                     printf("Pila vacia\n");
+                }
                 break;
-                
-            case 3: 
-                if(!vacia(tope))
+
+            case 3:
                 mostrar(tope);
-                else printf("Pila vacia\n");
                 break;
-            case 4: 
+
+            case 4:
+                printf("Saliendo del programa...\n");
                 break;
+
+            default:
+                printf("Opcion invalida\n");
         }
     } while(op != 4);
+
+    return 0;
 }
